d1ae43e90233b70e34b26b0ed74bfe66ec9492ea
Tom Downey <tom.downey@sky.uk>
2023-10-16 11:55:46 +0100

    example data format - data missing for security

---
+++ google_chatbot/examples.py
@@ -0,0 +1,12 @@
+from vertexai.language_models import InputOutputTextPair
+
+
+examples=[
+        InputOutputTextPair(
+            input_text="input text 1",
+            output_text="output text 1"
+        ),
+        InputOutputTextPair(
+            input_text="input text 2",
+            output_text="output text 2"
+        )]
